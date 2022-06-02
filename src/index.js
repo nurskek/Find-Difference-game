67ab8a56d504cb4eec9fc0fed8fefb52b7583ef6
Nursultan <kobeisinov@unist.ac.kr>
2022-06-02 21:31:39 +0900

    circle finding

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import img1 from './pictures/img_difference.svg';
 import img2 from './pictures/img_original.svg';
-
+import {useState} from 'react'
 
 
 // requirements:
@@ -15,23 +15,48 @@ import img2 from './pictures/img_original.svg';
 // better UI, more points
 // use React
 
+function Difference(props){
+
+    const [showCircle, setShowCircle] = useState(0)
+    const changeCircle = () => {
+        setShowCircle(1)
+    }
+
+    return(
+        <div style = {{opacity: showCircle}} className={props.className} onClick={(e) => changeCircle()}>
+        </div>
+    )
+}
+
+// function that show X when clicked
+
 class App extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         // there should be some components
-    //     }
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            total: 5,
+        }
+    }
+
     render() {
         return (
             <div className="App">
                 <h1>Find <span id="dif_num">3</span> differences</h1>
-                <div className="img_diff">
-                    <img src={img1} alt="Different image"/>
+                 
+                <div className="row">
+                    <div className="column">
+                        {/* <div style={{ opacity: ourCircle }} className="circle c1" onClick={(e) => this.rightClick()}></div> */}
+                        <Difference className="circle c1"/>
+                        <Difference className="circle c2"/>
+                        <img id="img_diff" src={img1} alt="Different image"/>
+                    </div>
+                    <div className="column">
+                        <Difference className="circle c1"/>
+                        <Difference className="circle c2"/>
+                        <img id="img_orig" src={img2} alt="Original image"/>
+                    </div>
                 </div>
-                <div className="img_orig">
-                    <img src={img2} alt="Original image"/>
-                </div>
+                    
             </div>
         );
     }
